15cb09483722c96c9ceccf855bed54a17bee0291
gennaromellone <gennaro.mellone1@studenti.uniparthenope.it>
2022-01-21 12:15:34 +0100

    Api fixed

--- src/app/gamification-template/gamification-template.controller.ts
+++ src/app/gamification-template/gamification-template.controller.ts
@@ -6,6 +6,8 @@ import { CrudRequest, ParsedRequest } from '@nestjsx/crud';
 import { TemplateDto } from './dto/import.dto';
 import { GamificationLayerService } from '../gamification-layers/gamification-layer.service';
 import fetch from 'node-fetch';
+import { Octokit } from '@octokit/core';
+import {Open} from 'unzipper';
 
 @ApiTags('gamification-template')
 @Controller('gamification-template')
@@ -19,7 +21,49 @@ export class GamificationTemplateController {
 
     @Get('/templates-list')
     async getAllTemplates() {
-        return [{ 'id': '4exercises' }, { 'id': '2exercises' }];
+        const octokit = new Octokit();
+
+        // Read github 'zip' directory content
+        const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+            owner: 'uniparthenope-fgpe',
+            repo: 'gamification-template',
+            path: 'zip'
+        });
+        const info = [];
+        const _data = response.data;
+
+        // For each template file
+        // tslint:disable-next-line:forin
+        for (const idx in _data) {
+            let count_ex = 0;
+            let count_ch = 0;
+
+            // Open zip file
+            const obj = await (await fetch(_data[idx].download_url)).buffer();
+            const directory = await Open.buffer(obj);
+
+            const files = directory.files;
+            for (const fdx in files) {
+                // Count exercises
+                if (files[fdx].type === 'File' && (files[fdx].path).includes('exercises/')) {
+                    count_ex++;
+                } else if (files[fdx].type === 'Directory' && (files[fdx].path).includes('challenges/')) {
+                    const challenges_path = (files[fdx].path.replace('challenges/', '')).split('/');
+                    if (challenges_path.length === 2) {
+                        count_ch++;
+                    }
+                    console.log(challenges_path);
+                    console.log(challenges_path.length);
+                }
+            }
+
+            info.push({
+                'id': (_data[idx].name).replace('.zip', ''),
+                'tot_exercises': count_ex,
+                'tot_challenges': count_ch
+            });
+        }
+        return info;
     }
 
     @Post('/create-from-template')
@@ -32,12 +76,12 @@ export class GamificationTemplateController {
         const url = 'https://raw.githubusercontent.com/uniparthenope-fgpe/gamification-template/main/' + dto.template_id + '.zip';
         const obj = await (await fetch(url)).buffer();
         const data = {'buffer': obj};
-
         const exercises_map = {};
         const exercises = dto.exercise_ids;
         exercises.forEach((exercise, i) => {
             exercises_map['EX_' + ++i] = exercise['id'];
         });
+
         return await this.gamificationService.import(user, dto.project_id, data, exercises_map);
     }
 }
